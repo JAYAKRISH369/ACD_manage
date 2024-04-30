82a77fad82a501266abdb0220869d18262f5c482
JAYAKRISHNA YELLAKARA <yjayakrish@gmail.com>
2024-05-01 00:17:45 +0530

    footer changed

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,9 +1,14 @@
 import "./CodeSection.css";
 function Footer(){
     return(
+        <>
+<div className="Footer_div">
+    JK presents..
+</div>
         <div className="Footer_div">
         It's your Healper... go_to_Hell
         </div>
+        </>
     );
 }
 
